152274a8e0d85dd94ae307fe2151b5928998b8b4
Gabriel Cenciati <gbrcenciati@gmail.com>
2023-01-21 14:36:57 -0300

    add: database connection handler first tests

---
+++ tests/integration/test_db_connection_handler.py
@@ -0,0 +1,28 @@
+# pylint: disable=line-too-long, protected-access, no-name-in-module, broad-except
+# flake8: noqa
+
+from sqlalchemy.engine.base import Engine
+
+from src.infrastructure.database.sqlalchemy.db_connection_handler import (
+    DBConnectionHandler,
+)
+
+
+def test_get_engine_should_return_sql_alchemy_new_engine_object() -> None:
+    # Arrange
+    db_connection_handler = DBConnectionHandler()
+
+    # Act
+    new_engine: Engine = db_connection_handler.get_engine()
+
+    # Assert
+    assert isinstance(new_engine, Engine)
+
+
+def test_when_enter_into_a_new_database_session_context_should_not_raise_an_error() -> None:
+    with DBConnectionHandler() as db_connection_handler:
+        try:
+            db_connection_handler.session.close()
+            assert True
+        except Exception:
+            assert False
