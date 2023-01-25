c5ddf4f684abb8fd0b73a3583c0e5880eb14ff6e
Gabriel Cenciati <gbrcenciati@gmail.com>
2023-01-25 18:37:44 -0300

    ci: create need_dot_env flag

--- tests/integration/infrastructure/test_db_connection_handler.py
+++ tests/integration/infrastructure/test_db_connection_handler.py
@@ -1,6 +1,7 @@
 # pylint: disable=line-too-long, protected-access, no-name-in-module, broad-except
 # flake8: noqa
 
+import pytest
 from sqlalchemy.engine.base import Engine
 
 from src.infrastructure.database.sqlalchemy.db_connection_handler import (
@@ -8,6 +9,7 @@ from src.infrastructure.database.sqlalchemy.db_connection_handler import (
 )
 
 
+@pytest.mark.need_dot_env
 def test_get_engine_should_return_sql_alchemy_new_engine_object() -> None:
     # Arrange
     db_connection_handler = DBConnectionHandler()
@@ -19,6 +21,7 @@ def test_get_engine_should_return_sql_alchemy_new_engine_object() -> None:
     assert isinstance(new_engine, Engine)
 
 
+@pytest.mark.need_dot_env
 def test_when_enter_into_a_new_database_session_context_should_not_raise_an_error() -> None:
     with DBConnectionHandler() as db_connection_handler:
         try:
